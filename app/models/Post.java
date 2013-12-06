39dc69eca24da6dfc4c29a6e2190266cd6eec7c1
Administrator <Administrator@Edwin-Home>
2013-12-06 22:26:01 +0800

    Home1206

---
+++ app/models/Post.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+
+@Entity
+public class Post extends Model {
+
+    public String title;
+    public Date postedAt;
+
+    @Lob
+    public String content;
+
+    @ManyToOne
+    public User author;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    public List<Comment> comments;
+
+    public Post(User author, String title, String content) {
+        this.comments = new ArrayList<Comment>();
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.postedAt = new Date();
+    }
+}
